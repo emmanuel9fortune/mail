3d632a8fff79b2dcbcc2c82bbe509d0915721af3
emmanuel fortune <108401233+emmanuel9fortune@users.noreply.github.com>
2024-09-23 01:59:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS configuration to allow requests from specific origins
 const corsOptions = {
-    origin: 'http://localhost:3000',  // Allow requests from this origin
+    origin: 'https://capitradx.com',  // Allow requests from this origin
     credentials: true,                 // Enable credentials (cookies, authorization headers, etc.)
     methods: ['GET', 'POST', 'OPTIONS'],       // Allowed methods
     allowedHeaders: 'Content-Type, Authorization' // Allowed headers
